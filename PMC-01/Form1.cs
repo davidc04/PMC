b6bb41e6047554635484d4e965813020d1ce46a1
davidc04 <30379801+davidc04@users.noreply.github.com>
2017-08-27 14:05:51 -0700

    Cambio completo de Userform1

--- PMC-01/Form1.cs
+++ PMC-01/Form1.cs
@@ -16,5 +16,30 @@ namespace PMC_01
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click_1(object sender, EventArgs e)
+        {
+
+        }
     }
 }
