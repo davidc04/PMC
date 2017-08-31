19c7a6ef80bdd48023361cbe3f3d1041577ed591
davidc04 <30379801+davidc04@users.noreply.github.com>
2017-08-30 17:05:15 -0700

    Cambio de layout

--- PMC-01/Form1.Designer.cs
+++ PMC-01/Form1.Designer.cs
@@ -42,6 +42,11 @@
             this.Estacionamiento = new System.Windows.Forms.CheckBox();
             this.label6 = new System.Windows.Forms.Label();
             this.Bodega = new System.Windows.Forms.CheckBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // Guardar_Inquilino
@@ -49,7 +54,7 @@
             this.Guardar_Inquilino.BackColor = System.Drawing.Color.Transparent;
             this.Guardar_Inquilino.FlatStyle = System.Windows.Forms.FlatStyle.System;
             this.Guardar_Inquilino.Font = new System.Drawing.Font("Candara", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.Guardar_Inquilino.Location = new System.Drawing.Point(38, 265);
+            this.Guardar_Inquilino.Location = new System.Drawing.Point(225, 291);
             this.Guardar_Inquilino.Name = "Guardar_Inquilino";
             this.Guardar_Inquilino.Size = new System.Drawing.Size(154, 27);
             this.Guardar_Inquilino.TabIndex = 0;
@@ -175,13 +180,63 @@
             this.Bodega.TabIndex = 14;
             this.Bodega.UseVisualStyleBackColor = true;
             // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.BackColor = System.Drawing.Color.Transparent;
+            this.label7.Font = new System.Drawing.Font("Candara", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(275, 32);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(74, 19);
+            this.label7.TabIndex = 15;
+            this.label7.Text = "Vehículos";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(382, 72);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(142, 20);
+            this.textBox1.TabIndex = 16;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(382, 37);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(15, 14);
+            this.checkBox1.TabIndex = 17;
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.BackColor = System.Drawing.Color.Transparent;
+            this.label8.Font = new System.Drawing.Font("Candara", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(275, 73);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(74, 19);
+            this.label8.TabIndex = 18;
+            this.label8.Text = "Vehículos";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(382, 112);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(142, 20);
+            this.textBox2.TabIndex = 19;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.InactiveCaption;
             this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
-            this.ClientSize = new System.Drawing.Size(231, 304);
+            this.ClientSize = new System.Drawing.Size(595, 330);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label7);
             this.Controls.Add(this.Bodega);
             this.Controls.Add(this.label6);
             this.Controls.Add(this.Estacionamiento);
@@ -225,8 +280,11 @@
         private System.Windows.Forms.CheckBox Estacionamiento;
         private System.Windows.Forms.Label label6;
         private System.Windows.Forms.CheckBox Bodega;
-
-        
+        private System.Windows.Forms.Label label7;
+        public System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox textBox2;
     }
 }
 
